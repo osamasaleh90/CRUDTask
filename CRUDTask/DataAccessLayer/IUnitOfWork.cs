41db419c9a316e69ee5c9ead4445c63e4161ac5e
osamasaleh90 <39797324+osamasaleh90@users.noreply.github.com>
2020-09-22 14:57:47 +0200

    -- Fix Connection and db creation

--- CRUDTask/DataAccessLayer/IUnitOfWork.cs
+++ CRUDTask/DataAccessLayer/IUnitOfWork.cs
@@ -20,6 +20,7 @@ namespace CRUDTask.DataAccessLayer
         public ApplicationDbContext()
             : base("DefaultConnection", throwIfV1Schema: false)
         {
+            Database.SetInitializer<ApplicationDbContext>(new CreateDatabaseIfNotExists<ApplicationDbContext>());
         }
 
         public static ApplicationDbContext Create()
